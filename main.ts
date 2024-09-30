03e979cc81b1ada4ab498e9a02299fe05aefe070
Scott Tomaszewski <scottTomaszewski@gmail.com>
2024-09-29 20:30:48 -0400

    Updates main to handle negotiation

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import {Plugin} from 'obsidian';
 import {PowerRollProcessor} from "./src/drawSteelAdmonition/powerRollProcessor";
 import {HorizontalRuleProcessor} from "./src/drawSteelAdmonition/horizontalRuleProcessor";
 import {InitiativeProcessor} from "./src/drawSteelAdmonition/initiativeProcessor";
+import {NegotiationTrackerProcessor} from "./src/drawSteelAdmonition/negotiationTrackerProcessor";
 
 export default class DrawSteelAdmonitionPlugin extends Plugin {
 	async onload() {
@@ -21,6 +22,10 @@ export default class DrawSteelAdmonitionPlugin extends Plugin {
 		const initHandler = (source, el, ctx) => initProcessor.postProcess(source, el, ctx);
 		this.registerMarkdownCodeBlockProcessor("ds-init", initHandler);
 		this.registerMarkdownCodeBlockProcessor("ds-initiative", initHandler);
+
+		let ntProcessor = new NegotiationTrackerProcessor();
+		this.registerMarkdownCodeBlockProcessor("ds-nt", ntProcessor.postProcess);
+		this.registerMarkdownCodeBlockProcessor("ds-negotiation-tracker", ntProcessor.postProcess);
 	}
 
 	onunload() {
