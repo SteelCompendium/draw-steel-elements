578114fdbe25101bb74a862a145f46d124fbc315
Alexander Bøhler <45562910+seedback@users.noreply.github.com>
2025-08-25 01:17:43 +0200

    Add CollapsibleHeading
    CollapsibleHeading should have a arrow indicator that has feature parity with Obsidian's own, but the hiding functionality has to be implemented in a parent component using the toggle event.

--- src/drawSteelComponents/SkillList/SkillGroup.vue
+++ src/drawSteelComponents/SkillList/SkillGroup.vue
@@ -1,51 +1,64 @@
 <template>
   <div class="container">
-		<h3>{{ toProperCase(props.groupName) }}</h3>
-		<ul class="skill-list" v-if="props.skillInfo">
-			<li class="skill-item" v-for="skill in props.skillInfo" :key="skill.name">
-				<toggle-indicator :enabled="hasSkill(skill.name)"></toggle-indicator>
-				<span :title="skill.use">{{ toProperCase(skill.name) }}</span>
-			</li>
-		</ul>
+        <collapsible-heading :header-level="3" :enabled="!isHeadingCollapsed" @toggle="handleToggle">
+            {{ toProperCase(props.groupName) }}
+        </collapsible-heading>
+        <ul class="skill-list" v-if="props.skillInfo && !isHeadingCollapsed">
+            <li class="skill-item" v-for="skill in props.skillInfo" :key="skill.name">
+                <toggle-indicator :enabled="hasSkill(skill.name)"></toggle-indicator>
+                <span :title="skill.use">{{ toProperCase(skill.name) }}</span>
+            </li>
+        </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import CollapsibleHeading from "@drawSteelComponents/Common/CollapsibleHeading.vue"
 import ToggleIndicator from "@drawSteelComponents/Common/ToggleIndicator.vue"
 import { SkillInfo } from "@utils/SkillsData"
 import { toProperCase } from "@utils/common";
+import { ref } from "vue";
 
 const props = defineProps<{
-	groupName?: string,
-	skillInfo?: SkillInfo[],
-	activeSkills?: string[]
+    groupName?: string,
+    skillInfo?: SkillInfo[],
+    activeSkills?: string[]
 }>()
 
+
+const isHeadingCollapsed = ref(false)
+
+
 const hasSkill = (skillName: string) => {
-	// Check if the skill is in the skills array (case-insensitive)
-	return props.activeSkills?.some((skill) => skill.toLowerCase() === skillName.toLowerCase()) ?? false;
+    // Check if the skill is in the skills array (case-insensitive)
+    return props.activeSkills?.some((skill) => skill.toLowerCase() === skillName.toLowerCase()) ?? false;
+}
+
+const handleToggle = () => {
+    isHeadingCollapsed.value = !isHeadingCollapsed.value
+    console.log("clicked", isHeadingCollapsed.value)
 }
 </script>
 
 <style scoped>
 .skill-group-container {
-	width: 100%;
+    width: 100%;
 }
 
 .title {
-	font-size: 1.2em;
-	margin-bottom: 0.5em;
-	margin-top: 0;
+    font-size: 1.2em;
+    margin-bottom: 0.5em;
+    margin-top: 0;
 }
 
 .skill-list {
-	list-style: none;
-	padding: 0;
+    list-style: none;
+    padding: 0;
 }
 
 .skill-item {
-	display: flex;
-	align-items: center;
-	margin-bottom: 0.3em;
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.3em;
 }
 </style>
--- src/drawSteelComponents/SkillList/SkillList.vue
+++ src/drawSteelComponents/SkillList/SkillList.vue
@@ -2,6 +2,7 @@
     <div class="container">
         <skill-group
             v-for="(skillInfo, groupName) in SKILL_DATA"
+            :key="groupName.toString()"
             :group-name="groupName.toString()"
             :skill-info="skillInfo"
             :activeSkills="activeSkills">
@@ -13,20 +14,21 @@
 import SkillGroup from "@drawSteelComponents/SkillList/SkillGroup.vue";
 import { SKILL_DATA } from "@utils/SkillsData"
 import { Skills } from "@model/Skills";
+import { ref } from "vue";
 
 const props = defineProps<{
     skills?: Skills,
 }>()
 
-let activeSkills = props.skills?.skills ?? []
-let fullSkillData = SKILL_DATA
+let activeSkills = ref(props.skills?.skills ? [...props.skills.skills] : []);
+let fullSkillData = ref(structuredClone(SKILL_DATA));
 
 for (let customSkill of props.skills?.custom_skills ?? []) {
-    let skillGroup = fullSkillData[customSkill.skill_group?.toLowerCase() ?? "ungrouped skills"]
-	skillGroup.push({name: customSkill.name, use: customSkill.description ?? ""})
-	if (customSkill.has_skill) {
-		activeSkills.push(customSkill.name)
-	}
+    let skillGroup = fullSkillData.value[customSkill.skill_group?.toLowerCase() ?? "ungrouped skills"]
+    skillGroup.push({name: customSkill.name, use: customSkill.description ?? ""})
+    if (customSkill.has_skill) {
+        activeSkills.value.push(customSkill.name)
+    }
 }
 
 </script>
